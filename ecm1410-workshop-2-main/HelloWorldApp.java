7963fb3eafd331b2feda16a59db84816fc26fb99
TomBBuoe <tjb248@exeter.ac.uk>
2026-01-30 18:50:41 +0000

    changed helloworldapp

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -3,6 +3,6 @@
 // HelloWorld application
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! My name is " + args[0] + ".");
     }
 }
